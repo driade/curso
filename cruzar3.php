8be4618ace9edd56750429db036a44505a4017c9
David Fernández <david@mundosaparte.com>
2022-04-12 23:06:06 +0200

    no message

---
+++ cruzar3.php
@@ -0,0 +1,19 @@
+<?php
+
+$empleados = [
+    [
+        'nombre' => 'Juan', 'edad' => 24,
+    ],
+    [
+        'nombre' => 'María', 'edad' => 26,
+    ],
+];
+
+$ausencias = [
+    [
+        'nombre' => 'Juan', 'ausencias' => 4,
+    ],
+    [
+        'nombre' => 'María', 'ausencias' => 0,
+    ],
+];
